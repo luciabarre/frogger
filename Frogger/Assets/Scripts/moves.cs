4405c21e37cf336b2e2c68c8423e5b1ff60e0f63
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-03-09 23:51:04 -0600

    fianl changes not important

--- Frogger/Assets/Scripts/moves.cs
+++ Frogger/Assets/Scripts/moves.cs
@@ -8,6 +8,15 @@ public class moves : MonoBehaviour
     [SerializeField] float limitx;
     [SerializeField] float limity;
     Vector3 move;
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite idleSprite;
+    public Sprite leapSprite;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,22 +26,23 @@ public class moves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move.x=Input.GetAxis("Horizontal");
+         move.x=Input.GetAxis("Horizontal");
          move.y=Input.GetAxis("Vertical");
+         
         //Debug.Log("X motion: "+ move.x);
 
-        if(transform.position.x < -limitx && move.x < 0){
-            move.x=0;
-        }
+         if(transform.position.x < -limitx && move.x < 0){
+             move.x=0;
+         }
         else if(transform.position.x > limitx && move.x > 0){
             move.x=0;
-        }
-        if(transform.position.y < -limity && move.y < 0){
-            move.y=0;
-        }
+         }
+         if(transform.position.y < -limity && move.y < 0){
+             move.y=0;
+         }
         else if(transform.position.y > limity && move.y > 0){
-            move.y=0;
-        }
+             move.y=0;
+         }
         //Esta función permite deterimanr la velocidad para que lado en el tiempo transcurrido.
         transform.Translate(move * speed * Time.deltaTime);
     }
